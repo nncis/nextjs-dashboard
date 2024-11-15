6b1702b677793539e112632ff793be3c973db7f3
nncis <115885591+nncis@users.noreply.github.com>
2024-11-15 09:54:49 -0300

    fix: try fix deployment

--- app/page.tsx
+++ app/page.tsx
@@ -37,6 +37,7 @@ export default function Page() {
             width={1000}
             height={760}
             className="hidden md:block"
+            priority={true} 
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
